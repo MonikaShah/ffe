61c5065e1e1a873c4e45ca05432fc46c8f8346d8
monikashah <monikapatira@gmail.com>
2026-01-22 14:53:05 +0530

    Updated urls.py

--- ffe/urls.py
+++ ffe/urls.py
@@ -24,6 +24,8 @@ from wagtail import urls as wagtail_urls
 urlpatterns = [
     path("admin/", admin.site.urls),
 
+    path("", views.home, name="home"),
+
     path(
         "login/",
         auth_views.LoginView.as_view(template_name="login.html"),
