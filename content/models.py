05bdfa0ed22dc9547a57b0e05b46fc7be90511d9
monikashah <monikapatira@gmail.com>
2026-01-23 16:04:31 +0530

    Added media updates page

--- content/models.py
+++ content/models.py
@@ -1,9 +1,10 @@
 from wagtail.models import Page
 from wagtail.fields import StreamField, RichTextField
 from wagtail import blocks
+
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.admin.panels import FieldPanel
-from wagtail.blocks import StructBlock, CharBlock, RichTextBlock
+from wagtail.blocks import StructBlock, CharBlock, RichTextBlock,URLBlock
 
 class HomePage(Page):
     """
@@ -67,6 +68,55 @@ class AchievementPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("intro"),
         FieldPanel("achievements"),
+    ]
+
+    parent_page_types = ["wagtailcore.Page"]
+    subpage_types = []
+
+class MediaUpdateBlock(StructBlock):
+    title = CharBlock(
+        required=True,
+        help_text="Media headline / title"
+    )
+
+    media_link = URLBlock(
+        required=False,
+        help_text="Link to news article (optional)"
+    )
+
+    video_embed = RichTextBlock(
+        required=False,
+        features=["embed"],
+        help_text="Paste YouTube / video embed"
+    )
+
+    thumbnail = ImageChooserBlock(
+        required=False,
+        help_text="Optional thumbnail image"
+    )
+
+    class Meta:
+        icon = "media"
+        label = "Media Update"
+
+
+class MediaUpdatesPage(Page):
+    intro = RichTextField(
+        blank=True,
+        features=["bold", "italic", "link"]
+    )
+
+    media_updates = StreamField(
+        [
+            ("media", MediaUpdateBlock()),
+        ],
+        blank=True,
+        use_json_field=True
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro"),
+        FieldPanel("media_updates"),
     ]
 
     parent_page_types = ["wagtailcore.Page"]
